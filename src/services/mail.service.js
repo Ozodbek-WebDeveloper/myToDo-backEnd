da347c428c3afe8b73c22dc2de2f4f9ffc18495a
_ozodbek <ozodbekpanjiyev0444@gmail.com>
2025-11-17 18:22:57 +0500

    nodeMailer bug fix

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -5,7 +5,7 @@ class mailService {
     this.transport = nodeMailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: true,
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
